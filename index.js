9b873a18abcae475918be3fed7a94b71d96fd198
MikoKong <128753361+MikoKong@users.noreply.github.com>
2026-03-03 18:43:53 -0800

    Allow user to input city

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-fetch("https://api.open-meteo.com/v1/forecast?latitude=47.6062&longitude=-122.3321&daily=weather_code,sunrise,sunset,precipitation_probability_max,precipitation_hours,precipitation_sum&hourly=temperature_2m,precipitation_probability&current=temperature_2m,rain,is_day,relative_humidity_2m")
-    .then((response) => {
-        if(!response.ok) {
-            throw new Error(response.status);
+document.getElementById('checkWeather').addEventListener('click', async() =>{
+    const cityName = document.getElementById('cityName').value.trim();
+    if(!cityName) {
+        alert("Please check the city name");
+        return;
+    }
+    try {
+        const geoResponse = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(cityName)}&count=1`);
+        if(!geoResponse.ok) {
+            throw new Error(geoResponse.status);
         }
-        return response.json();
-    })
-    .then((data) => {
-        const location = document.getElementById('location');
-        location.textContent += `Latitude: ${data.latitude} Longitude: ${data.longitude}`;
+        const geoData = await geoResponse.json();
+    
+        const latitude = geoData.results[0].latitude;
+        const longitude = geoData.results[0].longitude;
+        const userCityName = geoData.results[0].name;
 
-        const rain = document.getElementById('rain');
-        rain.textContent += `Current rainfall :${data.current.rain}`;
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=precipitation`);
+        if(!weatherResponse.ok) {
+            throw new Error(weatherResponse.status);
+        }
+        const weatherData = await weatherResponse.json();
 
-        const tempe = document.createElement('p');
-        tempe.textContent = `Current temperature :${data.current.temperature_2m}°F`
-        document.body.appendChild(tempe);
-    })
-    .catch((error) =>{
+        const rain = weatherData.hourly.precipitation[0];
+        const temperature = weatherData.current_weather.temperature;
+
+        document.getElementById('location').textContent = `Location: ${userCityName}`;
+        document.getElementById('rain').textContent = `Current rainfall: ${rain} mm`;
+        document.getElementById('temperature').textContent = `Current temperature: ${temperature}°C`;
+    } catch(error) {
         console.error(error);
-    });
+    }
+});
